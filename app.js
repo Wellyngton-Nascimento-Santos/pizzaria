49b154c46e68c096de35e36dd1c1ca1f2978c444
Wellyngton Nascimento <wellyngton.nsantos1910@gmail.com>
2022-11-09 11:35:30 -0300

    Começamos o projeto Pizzaria

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-
+// 1 - Importar o express
 const express = require('express');
+const path = require('path');
 
 
+// 2 - Criar o servidor
 const servidor =express();
 
+// Define a poasta public como sendo a pasta arquivos estáticos
+servidor.use(express.static(path.join(__dirname, 'public')))
 
-
-servidor.get('/usuarios', (req,res)=>{
+// 3 - Definir de uma rota neste servidor
+// endereço, método, função callback (a ação que o servidor vai realizar quando re)
+servidor.get('/', (req,res)=>{
     console.log("Chegou um requisição!");
     //return res.send("Vou te mandar uma lista de usuarios!");
     return res.sendFile(__dirname + "/views/index.html");
+    
 });
 
+servidor.get('/carrinho', (req,res)=>{
+   return res.sendFile(__dirname + "/views/carrinho.html");
+})
 
 
 
+// 4 - Por o servidor no modo "aguardando requisições"
 servidor.listen(3000);
+
+//servidor.get('/logo', (req,res)=>{
+//   return res.sendFile(__dirname + "/public/img/logo.svg");
+//})
