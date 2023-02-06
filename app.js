b7458e66039c11e37da9cbb2a1c5ebf8f8618d35
Wellyngton Nascimento <wellyngton.nsantos1910@gmail.com>
2023-02-06 09:14:04 -0300

    Atualizando

--- app.js
+++ app.js
@@ -1,40 +1,16 @@
 // 1 - Importar o express
 const express = require('express');
 const path = require('path');
-
+const router = require('./router');
 
 // 2 - Criar o servidor
-const servidor =express();
+const servidor = express();
 
-// Define a poasta public como sendo a pasta arquivos estáticos
+// Define a pasta public como sendo a pasta arquivos estáticos
 servidor.use(express.static(path.join(__dirname, 'public')))
 
-// 3 - Definir de uma rota neste servidor
-// endereço, método, função callback (a ação que o servidor vai realizar quando re)
-servidor.get('/', (req,res)=>{
-    console.log("Chegou um requisição!");
-    //return res.send("Vou te mandar uma lista de usuarios!");
-    return res.sendFile(__dirname + "/views/index.html");
-    
-});
+// 3 - Definir roteador a ser utilizado
+servidor.use(router);
 
-servidor.get('/carrinho', (req,res)=>{
-   return res.sendFile(__dirname + "/views/carrinho.html");
-});
-
-servidor.get('/perfil', (req,res)=>{
-    return res.sendFile(__dirname + "/views/perfil.html");
- });
-
- servidor.get('/cadastro', (req,res)=>{
-     return res.sendFile(__dirname + "/views/cadastro.html");
-  })
-
-
-
-// 4 - Por o servidor no modo "aguardando requisições"
+// 4 - Por o servidor no modo "aguardando requisição"
 servidor.listen(3000);
-
-//servidor.get('/logo', (req,res)=>{
-//   return res.sendFile(__dirname + "/public/img/logo.svg");
-//})
